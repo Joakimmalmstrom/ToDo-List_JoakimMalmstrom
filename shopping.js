b9521ab856b95e28f48f522a45d59cd1ede1716b
joakimmalmstrom <joakim.s.malmstrom@gmail.com>
2020-10-01 15:28:57 +0200

    Get hovering to work but not selected

--- shopping.js
+++ shopping.js
@@ -1,13 +1,15 @@
 const submitButton = document.getElementById("my-submit");
+const removeButton = document.getElementById("my-remove");
 const selectBox = document.getElementById("my-select");
+const selected = document.getElementById("my-select").selectedIndex;
 
-submitButton.addEventListener("click", function() {
+submitButton.addEventListener("click", function () {
 
-    const userInput = document.getElementById("product").value;
+    const userInput = document.getElementById("my-input").value;
 
     // Create option element
     let opt = document.createElement("option");
-    opt.setAttribute("value", "input-product");
+    opt.setAttribute("value", "product");
     // Create text node with the user input || Assign text to option element
     let input = document.createTextNode(userInput);
     opt.appendChild(input);
@@ -15,6 +17,19 @@ submitButton.addEventListener("click", function() {
     selectBox.appendChild(opt);
 });
 
+removeButton.addEventListener("click", function () {
+
+    selectBox.remove(selectBox.selectedIndex);
+});
+
+selectBox.addEventListener("click", function () {
+    console.log("Clicked");
+
+    // var options = selectBox.querySelectorAll("option");
+    //     options.backgroundColor = "red";
+    //     console.log("Selected Index: " + options.values);
+    //     console.log("Options Length: " + options.length);
+});
 
 
 // BUTTON CLICK
