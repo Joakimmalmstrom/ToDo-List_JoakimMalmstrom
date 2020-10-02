e69113fa316b20fc8126ef39ce9cf1531417bd1f
joakimmalmstrom <joakim.s.malmstrom@gmail.com>
2020-10-02 14:16:22 +0200

    Remove implemented
    Remove button works.
    Hides remove button when no elements in the list.

--- shopping.js
+++ shopping.js
@@ -4,44 +4,39 @@ const list = document.getElementById("my-list");
 
 addButton.addEventListener("click", function () {
 
+    removeButton.classList.remove("d-none");
+
     const userInput = document.getElementById("my-input").value;
 
     let btn = document.createElement("button");
     btn.setAttribute("type", "button");
-    btn.setAttribute("class", "list-group-item list-group-item-action my-list-item");
+    btn.setAttribute("class", "list-group-item list-group-item-action");
+
+    let check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("class", "mr-2 checkboxes");
 
     let text = document.createTextNode(userInput);
+    btn.appendChild(check);
     btn.appendChild(text);
     list.appendChild(btn);
 });
 
 removeButton.addEventListener("click", function () {
 
-    // How do I know which child is selected?
-    // list.removeChild()
+    // Checks for all checked Checkboxes
+    let check = document.querySelectorAll("[type=checkbox]");
+    check.forEach(c => {
+
+        // if it's checked - remove c's parent
+        if (c.checked) {
+            list.removeChild(c.parentNode);
+        } 
+    });
+
+    // If there's no elements in the list - hide remove button
+    if (list.childElementCount <= 1)
+    {
+        removeButton.classList.add("d-none");
+    }
 });
-
-function removeButtonShow() {
-
-}
-
-
-// BUTTON CLICK
-
-// const button = document.getElementById("buttonID");
-
-// button.addEventListener("click", function() {
-
-//     const review = document.getElementById("review");
-
-//     if (review.classList.contains("d-none"))
-//     {
-//         review.classList.remove("d-none");
-//         button.textContent = "Close Review";
-//     }
-//     else
-//     {
-//         review.classList.add("d-none");
-//         button.textContent = "Open Review";
-//     }
-// });
